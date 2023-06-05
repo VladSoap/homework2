04091f9b04dd387b3558557e81732632c16cea8c
VladSoap <@pLUnkmdsV43TX9>
2023-06-05 07:30:15 +0300

    homework2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        int dayOfWeek = 2;
+        switch (dayOfWeek) {
+            case 1 -> System.out.println("Понедельник");
+            case 2 -> System.out.println("Вторник");
+            case 3 -> System.out.println("Среда");
+            case 4 -> System.out.println("Четверг");
+            case 5 -> System.out.println("Пятница");
+            case 6 -> System.out.println("Суббота");
+            case 7 -> System.out.println("Воскресенье");
+            default -> System.out.println("Неверный день недели");
         }
     }
+}
